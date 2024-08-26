c054376138891076deb955fc7ddcabf80f0e4f91
gonzaloro100 <gonzaloro100@gmail.com>
2024-08-26 17:28:09 +0200

    26/08
    01

---
+++ compilador01_08/compi01.java
@@ -0,0 +1,13 @@
+package compilador01_08;
+
+public class compi01 {
+	public static void main(String[] ar) {
+		System.out.println("Maria Teresa Novoa Vazquez");
+		System.out.println("C/ Sarmiento Rivera, 22, 1ro F");
+		System.out.println("Ponteareas");
+		System.out.println("");
+		System.out.println("Gonzalo Rodriguez Osorio");
+		System.out.println("C/ Virxen do Carmen, 32, 4to A");
+		System.out.println("Riveira");
+	}
+}
